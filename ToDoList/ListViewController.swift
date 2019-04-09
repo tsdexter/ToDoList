cbc88ee08590d89f6bf883624c4eda9a512faea0
Thomas <tsdexter@gmail.com>
2019-04-09 14:50:25 -0400

    end video 2

--- ToDoList/ListViewController.swift
+++ ToDoList/ListViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 
 class ListViewController: UIViewController {
     
+    var data = ["A", "B", "C", "D"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.addSubview(titleLabel)
+        view.addSubview(tableView)
         view.setNeedsUpdateConstraints()
     }
     
@@ -27,12 +30,48 @@ class ListViewController: UIViewController {
         return view
     }()
     
+    lazy var tableView: UITableView! = {
+        let view = UITableView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.separatorStyle = UITableViewCell.SeparatorStyle.singleLine
+        view.tableFooterView = UIView(frame: .zero)
+        view.contentInset = UIEdgeInsets.init(top: 0, left: 0, bottom: 40, right: 0)
+        
+        view.dataSource = self
+        view.delegate = self
+        view.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return view
+    }()
+    
     override func updateViewConstraints() {
         let margins = view.layoutMarginsGuide
         
         titleLabel.topAnchor.constraint(equalTo: margins.topAnchor, constant: 20).isActive = true
         titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        tableView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 5).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: margins.bottomAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
         super.updateViewConstraints()
     }
+}
+
+extension ListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+}
+
+extension ListViewController: UITableViewDelegate {
+    
 }
 
